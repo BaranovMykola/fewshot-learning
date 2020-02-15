7fadd10e0538f8602af86c981f153ad0474e0122
Mykola <m.baranov@abtollc.com>
2020-02-15 10:38:10 +0200

    WIP: Dataset

--- visualize_dataset.py
+++ visualize_dataset.py
@@ -5,6 +5,14 @@ from matplotlib.colors import LinearSegmentedColormap
 from src.dataset.fss import FssDataset
 
 
+def show_sample(ax, sample, name):
+    img = sample[:, :, :3]
+    mask = sample[:, :, 3]
+    ax.imshow(img.numpy()/255)
+    ax.imshow(mask.numpy()/255, cmap='rainbow_alpha', alpha=0.4)
+    ax.title.set_text(name)
+
+
 n_colors = 256
 
 color_array = plt.get_cmap('gist_rainbow')(range(n_colors))
@@ -17,18 +25,17 @@ plt.register_cmap(cmap=map_object)
 n_cols = 6
 n_rows = 6
 dataset = FssDataset('./fewshots.csv')
-test = dataset.create(dataset.test, 5)
+test = dataset.create(dataset.test, 5).shuffle(1024)
 
 
-fig, ax = plt.subplots(ncols=n_cols, nrows=n_rows, figsize=(n_cols * 5, n_rows * 5))
-# fig.suptitle('Sample examples', fontsize=16)
-ax = ax.flatten()
+fig, ax = plt.subplots(ncols=n_cols, nrows=n_rows, figsize=(n_cols * 4, n_rows * 4))
+fig.suptitle('Sample examples', fontsize=16)
+# ax = ax.flatten()
 
-for idx, (sample, classname, class_id) in enumerate(test.take(n_cols)):
-    img = sample[:, :, :3]
-    mask = sample[:, :, 3]
-    ax[idx].imshow(img.numpy()/255)
-    ax[idx].imshow(mask.numpy()/255, cmap='rainbow_alpha', alpha=0.4)
-    ax[idx].title.set_text(f'{classname.numpy().decode()} ({class_id})')
+for idx, (query, classname, class_id, support) in enumerate(test.take(n_cols)):
+    name = f'{classname.numpy().decode()} ({class_id})'
+    show_sample(ax[idx, 0], query, name)
+    for i in range(1, n_cols):
+        show_sample(ax[idx, i], support[i-1, :, :, :], f'Support {i}')
 
 plt.savefig('./dump/dataset_example.png')
